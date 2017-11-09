49dc0c8738abb0d9287ea94cf39c0c22e32129df
Mehmet Sunkur <mehmetsunkur@gmail.com>
2017-11-09 18:15:58 +0300

    configure webpack/webpack.common.js to copy assets folder to targt web folder to be able to served from application

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -96,7 +96,8 @@ module.exports = (options) => {
                 { from: './src/main/webapp/manifest.webapp', to: 'manifest.webapp' },
                 // { from: './src/main/webapp/sw.js', to: 'sw.js' },
                 // jhipster-needle-add-assets-to-webpack - JHipster will add/remove third-party resources in this array
-                { from: './src/main/webapp/robots.txt', to: 'robots.txt' }
+                { from: './src/main/webapp/robots.txt', to: 'robots.txt' },
+                { from: './src/main/webapp/app/ngx-admin/assets', to: 'assets' }
             ]),
             new webpack.ProvidePlugin({
                 $: "jquery",
